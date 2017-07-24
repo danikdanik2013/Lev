3d55d464750301cd613d3ff84b90e7a6334c014d
danikdanik2013 <danikdanik20166@gmail.com>
2017-07-24 22:58:09 +0300

    1

--- 1234/1234.py
+++ 1234/1234.py
@@ -2,14 +2,9 @@
 import csv
 
 x = '7.csv'
-infile = open(x, 'r')
-table = []
-for row in csv.reader(infile):
-    table.append(row)
 vvod = input("Model is: ")
 with open ( x , 'r' , newline = '' ) as f:
         file = csv.reader(f)
-        # table = [row for row in file]
         for row in file:
             if vvod == "Porsche 911":
                 print (vvod,'-',row[1])
@@ -28,21 +23,3 @@ with open ( x , 'r' , newline = '' ) as f:
 #     sortedlist = sorted(x, key=lambda row: (row[1],row[0],row[2]), reverse=True)
 # else:
 #     print (" okey...")
-# def aaa():
-#     with open ( x , 'r' , newline = '' ) as f:
-#         file = csv.reader(f)
-#         for row in file:
-#             print ( row[0])
-#             return (row[0])
-# def bbb():
-#     with open ( x , 'r' , newline = '' ) as f:
-#         file = csv.reader(f)
-#         for row in file:
-#             print ( row[1])
-#             return (row[1])
-# def ccc():
-#     with open ( x , 'r' , newline = '' ) as f:
-#         file = csv.reader(f)
-#         for row in file:
-#             print ( row[2])
-#   ..          return ( row [2])
