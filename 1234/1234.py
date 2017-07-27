b01d2909ca36ee20c14f8b2c9aa38b7b32927efb
danikdanik2013 <danikdanik20166@gmail.com>
2017-07-27 22:50:04 +0300

    1

--- 1234/1234.py
+++ 1234/1234.py
@@ -2,22 +2,13 @@
 import csv
 
 x = '7.csv'
-vvod = input("Model is: ")
+word_input = input("Model is: ")
 with open ( x , 'r' , newline = '' ) as f:
-        file = csv.reader(f)
-        for row in file:
-            if vvod == "Porsche 911":
-                print (vvod,'-',row[1])
-                break
-            elif vvod == "Porsche 918":
-                print (row[2])
-                break
-            elif vvod == "Porsche 906":
-                print (row[3])
-                break
-            else:
-                print("UNCORRECTS")
-                break
+    file = csv.reader(f)
+    for row in file:
+        if word_input == row[0]:
+            print (row[0],' - ', row[1])
+
 # input_sorted = input("Yes/No?: ")
 # if input_sorted == "Yes":
 #     sortedlist = sorted(x, key=lambda row: (row[1],row[0],row[2]), reverse=True)
