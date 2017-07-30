38a97cc76c3f50f91fd364d880f2c2cbea1d3f0b
danikdanik2013 <danikdanik20166@gmail.com>
2017-07-30 14:09:11 +0300

    1

--- 1234/1234.py
+++ 1234/1234.py
@@ -6,8 +6,10 @@ word_input = input("Model is: ")
 with open ( x , 'r' , newline = '' ) as f:
     file = csv.reader(f)
     for row in file:
+        a = row[0]
+        b = row[1]
         if word_input == row[0]:
-            print (row[0],' - ', row[1])
+            print ("{} - {}".format(a, b))
 
 # input_sorted = input("Yes/No?: ")
 # if input_sorted == "Yes":
