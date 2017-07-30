ae6795a03d513439be51e90806f9f301da146420
danikdanik2013 <danikdanik20166@gmail.com>
2017-07-30 18:56:48 +0300

    1

--- 1234/1234.py
+++ 1234/1234.py
@@ -9,17 +9,20 @@ if word_input2 == "yes":
         for row in file:
             a = row[0]
             print("{}".format(a))
-        word_input = input("Do you want get more information about this car?(yes/no)")
-        if word_input == "yes":
-            for row in file:
+        f.close()
+    word_input = input("Do you want get more information about this car?(yes/no)")
+    if word_input == "yes":
+            with open(x, 'r', newline='') as f:
+                file = csv.reader(f)
                 car_input = input("Model is: ")
-                a = row[0]
-                b = row[1]
-                if car_input == row[0]:
-                    print ("{} - {}".format(a, b))
-                    break
-        else:
-            print("12")
+                for row in file:
+                    a = row[0]
+                    b = row[1]
+                    if car_input == row[0]:
+                        print ("{} - {}".format(a, b))
+                        break
+    else:
+            print("OOOPS")
 
 
 
