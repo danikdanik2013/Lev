4e09500f9201606227dbcd01bcae45c68f86525e
danikdanik2013 <danikdanik20166@gmail.com>
2017-08-02 16:37:13 +0300

    1

--- 1234/1234.py
+++ 1234/1234.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
 import csv
-import pandas as pd
 
 x = '7.csv'
 word_input2 = input("Do you want to watch all list?(yes/no) - ")
 if word_input2 == "yes":
     with open ( x , 'r' , newline = '' ) as f:
         file = csv.reader(f)
+        a = []
         for row in file:
-            a = row[0]
-            print("{}".format(a))
-        f.close()
-    sort_input = input("Do you want to sorted list?(yes/no) ")
-    if sort_input == "yes":
-        with open(x, 'r', newline='') as f:
-            file = csv.reader(f)
-            df = pd.DataFrame(f)
-            for row in file:
-                print(df.sort_values('Alphabet'))
-                # break
-    else:
-        print("NOPE")
-    f.close()
+            b = row[0]
+            print("{}".format(b))
+            a.append(b)
+        sort_input = input("Do you want to sorted list?(yes/no) ")
+        if sort_input == "yes":
+            newList = sorted(a)
+            print("{}".format(newList))
+            f.close()
+
     word_input = input("Do you want get more information about this car?(yes/no) - ")
     if word_input == "yes":
             with open(x, 'r', newline='') as f:
@@ -35,12 +30,11 @@ if word_input2 == "yes":
                         break
     else:
             print("OOOPS")
-def sortByAlphabet(inputStr):
-                return inputStr[0]
+
 
 # input_sorted = input("Yes/No?: ")
 # if input_sorted == "Yes":
-#     sortedlist = sorted(x, key=lambda row: (row[1],row[0],row[2]), reverse=True)
+#     sortedlist = sorted(x, key=lambda row: (row[1],row[0],ro1w[2]), reverse=True)
 # else:
 #     print (" okey...")
 # word_input = input("Model is: ")
